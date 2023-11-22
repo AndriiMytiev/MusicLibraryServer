e48fdf618ed18cabdee16a5f86110eb90fec8516
AndriiMytiev <andrijmitev95@gmail.com>
2023-11-22 11:22:54 +0200

    update database.sql

--- database.sql
+++ database.sql
@@ -2,5 +2,10 @@ create TABLE users(
     id SERIAL PRIMARY KEY,
     login VARCHAR(100),
     password VARCHAR(50),
-    isAdmin BOOLEAN,
+    name VARCHAR(50),
+    surname VARCHAR(50),
+    info TEXT,
+    avatar TEXT,
+    favourites INTEGER[],
+    admin BOOLEAN,
 )
