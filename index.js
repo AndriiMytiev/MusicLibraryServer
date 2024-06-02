f099473d1d2dc1a3e2a205a01273ac3c9284e3fe
Andrii Mytiev <79248454+AndriiMytiev@users.noreply.github.com>
2024-06-02 20:45:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
+
 app.use(express.json());
 app.use("/api", userRouter);
 app.use("/api", musicRouter);
